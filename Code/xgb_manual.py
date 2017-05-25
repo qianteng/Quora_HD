e549d76bd2755b2949a560885570cd61a6494eb2
qteng <tengqian0001@gmail.com>
2017-05-25 15:10:15 -0400

    update xgb_manual.py

--- Code/xgb_manual.py
+++ Code/xgb_manual.py
@@ -22,12 +22,19 @@ X_train_cv = data_dict["X_train_basic"][splitter[i][0], :]
 X_valid_cv = data_dict["X_train_basic"][splitter[i][1], :]
 y_train_cv = data_dict["y_train"][splitter[i][0]]
 y_valid_cv = data_dict["y_train"][splitter[i][1]]
-params = {'objective': 'binary:logistic', 'eval_metric': 'logloss', 'eta': .02, 'max_depth': 7,
-          'silent': 1}
+#params = {'objective': 'binary:logistic', 'eval_metric': 'logloss', 'eta': .02, 'max_depth': 7, 'silent': 1}
+#params = {'base_score': 0.369197, 'booster': 'gbtree', 'colsample_bylevel': 0.4, 'colsample_bytree': 1,
+#          'gamma': 0.08759523291635393, 'eta': 0.072, 'max_depth': 8, 'min_child_weight': 1.6010959785458306e-10,
+#          'num_class': 2, 'objective': 'multi:softprob', 'alpha': 9.817830899287001,
+#          'lambda': 0.7386562144326775, 'seed': 2017, 'subsample': 0.95, 'silent': 1}
+params = {'base_score': 0.369197, 'booster': 'gbtree', 'colsample_bylevel': 0.4, 'colsample_bytree': 1,
+          'gamma': 0.08759523291635393, 'eta': 0.072, 'max_depth': 6, 'min_child_weight': 1.6010959785458306e-10,
+          'eval_metric': 'logloss', 'objective': 'binary:logistic', 'alpha': 9.817830899287001,
+          'lambda': 0.7386562144326775, 'seed': 2017, 'subsample': 0.85, 'silent': 1}
+num_round = 500
 d_train = xgb.DMatrix(X_train_cv, label=y_train_cv)
 d_valid = xgb.DMatrix(X_valid_cv, label=y_valid_cv)
 watchlist = [(d_train, 'train_cv'), (d_valid, 'valid_cv')]
-num_round = 10000
 bst = xgb.train(params, d_train, num_round, watchlist, early_stopping_rounds=50, verbose_eval=10)
 
 d_test = xgb.DMatrix(X_test)
@@ -35,6 +42,9 @@ p_test = bst.predict(d_test)
 sub = pd.DataFrame()
 test = pd.read_csv(config.TEST_DATA, encoding="ISO-8859-1")
 sub['test_id'] = test['test_id']
-sub['is_duplicate'] = p_test
+try:
+    sub['is_duplicate'] = p_test
+except:
+    sub['is_duplicate'] = p_test[:,1]
 path = os.path.join(config.OUTPUT_DIR + '/Subm', 'xgb_manual.csv')
 sub.to_csv(path, index=False)
