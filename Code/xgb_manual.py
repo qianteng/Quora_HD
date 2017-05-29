4e92239aec0bb8e10dacde7ffcbaa12c08940260
qteng <tengqian0001@gmail.com>
2017-05-29 12:29:54 -0400

    use original params in xgb_manual

--- Code/xgb_manual.py
+++ Code/xgb_manual.py
@@ -23,7 +23,6 @@ if combine_flag:
 feature_name = "basic_magic_v0"
 fname = os.path.join(config.FEAT_DIR+"/Combine", feature_name+config.FEAT_FILE_SUFFIX)
 data_dict = pkl_utils._load(fname)
-"""
 X_train = data_dict["X_train_basic"]
 X_test = data_dict["X_test"]
 y_train = data_dict["y_train"]
@@ -34,16 +33,14 @@ X_train_cv = data_dict["X_train_basic"][splitter[i][0], :]
 X_valid_cv = data_dict["X_train_basic"][splitter[i][1], :]
 y_train_cv = data_dict["y_train"][splitter[i][0]]
 y_valid_cv = data_dict["y_train"][splitter[i][1]]
-#params = {'objective': 'binary:logistic', 'eval_metric': 'logloss', 'eta': .02, 'max_depth': 7, 'silent': 1}
+params = {'objective': 'binary:logistic', 'eval_metric': 'logloss', 'eta': .02,
+          'max_depth': 7,'subsample': 0.6, 'base_score': 0.2, 'silent': True}
 #params = {'base_score': 0.369197, 'booster': 'gbtree', 'colsample_bylevel': 0.4, 'colsample_bytree': 1,
 #          'gamma': 0.08759523291635393, 'eta': 0.072, 'max_depth': 8, 'min_child_weight': 1.6010959785458306e-10,
 #          'num_class': 2, 'objective': 'multi:softprob', 'alpha': 9.817830899287001,
 #          'lambda': 0.7386562144326775, 'seed': 2017, 'subsample': 0.95, 'silent': 1}
-params = {'base_score': 0.369197, 'booster': 'gbtree', 'colsample_bylevel': 0.4, 'colsample_bytree': 1,
-          'gamma': 0.08759523291635393, 'eta': 0.072, 'max_depth': 6, 'min_child_weight': 1.6010959785458306e-10,
-          'eval_metric': 'logloss', 'objective': 'binary:logistic', 'alpha': 9.817830899287001,
-          'lambda': 0.7386562144326775, 'seed': 2017, 'subsample': 0.85, 'silent': 1}
-num_round = 1835
+
+num_round = 2500
 d_train_cv = xgb.DMatrix(X_train_cv, label=y_train_cv, feature_names = data_dict["feature_names"])
 d_valid_cv = xgb.DMatrix(X_valid_cv, label=y_valid_cv, feature_names = data_dict["feature_names"])
 watchlist = [(d_train_cv, 'train_cv'), (d_valid_cv, 'valid_cv')]
@@ -52,7 +49,7 @@ watchlist = [(d_train_cv, 'train_cv'), (d_valid_cv, 'valid_cv')]
 d_train = xgb.DMatrix(X_train, label=y_train, feature_names = data_dict["feature_names"])
 d_test = xgb.DMatrix(X_test, feature_names = data_dict["feature_names"])
 watchlist = [(d_valid_cv, 'valid_cv')]
-bst = xgb.train(params, d_train, num_round, watchlist, verbose_eval=50)
+bst = xgb.train(params, d_train, num_round, watchlist, early_stopping_rounds=50, verbose_eval=50)
 p_test = bst.predict(d_test)
 sub = pd.DataFrame()
 test = pd.read_csv(config.TEST_DATA, encoding="ISO-8859-1")
@@ -61,15 +58,14 @@ try:
     sub['is_duplicate'] = p_test
 except:
     sub['is_duplicate'] = p_test[:,1]
-path = os.path.join(config.OUTPUT_DIR + '/Subm', 'xgb_manual.csv')
+path = os.path.join(config.OUTPUT_DIR + '/Subm', 'xgb_magic_v0.csv')
 sub.to_csv(path, index=False)
 
 ax = xgb.plot_importance(bst)
 ax.figure.savefig("%s/%s.pdf"%(config.FIG_DIR, "feature_importance"))
 yticklabels = ax.get_yticklabels()[::-1]
 topn = len(yticklabels)
-fname = "XGBClassifier_topn_features_manual.txt"
+fname = "XGBClassifier_topn_features_magic.txt"
 with open(fname, "w") as f:
     for i in range(topn):
         f.write("%s\n"%yticklabels[i].get_text())
-"""
