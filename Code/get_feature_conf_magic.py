83cab1335cc3e2b2273d003344f1c3a2c23068ed
qteng <tengqian0001@gmail.com>
2017-05-29 22:50:07 -0400

    minor

--- Code/get_feature_conf_magic.py
+++ Code/get_feature_conf_magic.py
@@ -44,8 +44,9 @@ COMMENT_OUT_FEATS = [
 ]
 for file in sorted(os.listdir(config.FEAT_COMM_NONLINEAR_DIR)):  # add the first 0.3 of features in the files
     feature_list = pd.read_table(config.FEAT_COMM_NONLINEAR_DIR + '/' + file, header = None).values.flatten().tolist()
-    keep_ratio = 0.5
+    keep_ratio = 0.0
     MANDATORY_FEATS = MANDATORY_FEATS + feature_list[:int(len(feature_list) * keep_ratio)]
+print(MANDATORY_FEATS)
 
 def _check_include(fname):
     for v in INCLUDE_FEATS:
